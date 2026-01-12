50571110424921aa5e353e1dd724d1694f7059c8
r4hul078 <r4hul078@gmail.com>
2026-01-12 08:53:22 -0500

    Login/Registration

---
+++ Login/Registration/script.js
@@ -0,0 +1,9 @@
+function showRegister() {
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("register-form").style.display = "block";
+}
+
+function showLogin() {
+    document.getElementById("register-form").style.display = "none";
+    document.getElementById("login-form").style.display = "block";
+}
